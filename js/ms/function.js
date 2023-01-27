6c23f6b4d95efd3e4af239aca64c017a6583f028
Shun Muraki <shun.muraki.kendama@gmail.com>
2023-01-27 15:08:04 +0900

    adjusted

--- js/ms/function.js
+++ js/ms/function.js
@@ -1,12 +1,24 @@
+let default_pos;
+
+// styling_ クラスを取り外すことで content の位置を ms のスペースに対応させる関数.
 export const adjust_target_pos = (e, f) => {
     let ms_top = getComputedStyle(e).top;
-    let te = ms_top.substring(0, ms_top.length - 2);
+    console.log(ms_top);
     let ms_st;
     if (f == "on") {
-        ms_st = Number(te) + 30;
+        default_pos = Number(ms_top.substring(0, ms_top.length - 2));
+        if (default_pos > 60) {
+            ms_st = 60 + default_pos;
+        } else {
+            ms_st = 60;
+        }
+        let ms_st_code = ms_st + "px";
+        e.style.setProperty('top', ms_st_code, 'important');
     } else if (f == "off") {
-        ms_st = Number(te) - 30;
+        ms_st = default_pos;
     }
-    let ms_st_code = ms_st + "px";
-    e.style.setProperty('top', ms_st_code, 'important');
+    if (f == "off") {
+        e.style.top = '';
+        default_pos  = 0;
+    }
 }
